5c919a662f5a2fd3d5eb80c0cd102d7a04b4f938
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-02-01 00:39:22 -0500

    Change bot to client

--- bot.js
+++ bot.js
@@ -5,8 +5,8 @@ const Enmap = require('enmap');
 
 
 //initalize the client
-const bot = new Discord.Client();
-bot.login(config.token);
+const client = new Discord.Client();
+client.login(config.token);
 
 function newEnmap(name) {
 	return new Enmap({
@@ -16,42 +16,42 @@ function newEnmap(name) {
 		autoFetch: true, 
 		polling: true
 	});
-};
+}
 
 //ready event
 
-bot.on('ready', () => {
-	console.log(`I have logged in as ${bot.user.tag}.`);
+client.on('ready', () => {
+	console.log(`I have logged in as ${client.user.tag}.`);
 	console.log('I am now going to initalize myself for use.');
 	console.log('Now trying to set the status of the bot.');
-	bot.user.setActivity(`For @${bot.user.tag}`, { type: 'WATCHING'} );
+	client.user.setActivity(`For @${client.user.tag}`, { type: 'WATCHING'} );
 	console.log('I have set the status.')
 	console.log('I am now going to initialize the databases');
-	bot.economyData = newEnmap('economyData');
+	client.economyData = newEnmap('economyData');
 	console.log(`The economyData database has been started.`);
-	bot.cooldownData = newEnmap('cooldownData');
+	client.cooldownData = newEnmap('cooldownData');
 	console.log('The cooldownData database has been started and is listening at localhost:8080');
 	console.log(`I am now ready for use! I am completely started.`);
 	console.log(`I am now intializing the systemData databse.`);
-	bot.systemData = newEnmap('systemData');
+	client.systemData = newEnmap('systemData');
 	console.log(`System Data is ready.`);
 });
 
-bot.on('message', (message) => {
-	bot.economyData.ensure(`${message.author.id}-${message.guild.id}`, {
+client.on('message', (message) => {
+	client.economyData.ensure(`${message.author.id}-${message.guild.id}`, {
 		userID: message.author.id, 
 		linkCoins: 500, 
 		purchases: [], 
 		activityMeter: 0
 	})
-	bot.cooldownData.ensure(`${message.author.id}-${message.guild.id}`, {
+	client.cooldownData.ensure(`${message.author.id}-${message.guild.id}`, {
 		userID: message.author.id, 
 		activeCoolGuildID: message.guild.id,
 		commandsWithActiveCool: [], 
 		commandHashWithActiveCool: [], 
 		coolPardon: false,
 	});
-	bot.systemData.ensure(message.author.id, {
+	client.systemData.ensure(message.author.id, {
 		userID: message.author.id, 
 		userBlocked: false, 
 		grantEvalAccess: false, 
@@ -64,4 +64,4 @@ bot.on('message', (message) => {
 });
 
 
-handler(__dirname + '/commands', bot, { customPrefix: config.prefix } );
+handler(__dirname + '/commands', client, { customPrefix: config.prefix } );
