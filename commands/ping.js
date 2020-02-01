ec7fe489afa1afe44d8d779995ab4d7c2bf0c61a
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-01-31 20:44:10 -0500

    Create/Finish ping command ~ Need's improvement
    This make a complete ping command, however, this will need to be changed to include multiple numbers and not just the bot's roundtrip data

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,16 @@
+const Discord = require('discord.js');
 module.exports = {
     id: 'ping',
     aliases: ['pong'], // defaults to []
     channels: 'any', // defaults to 'any'. options are: 'dm', 'guild', 'any'.
     // 'call' is an instance of the Call class, a class containing various properties and utility functions.
     exec: (call) => {
-        call.message.channel.send('Pong! ' + Math.round(call.client.ping) + 'ms D-API delay.');
+        call.message.channel.send(`Ping? Give me a second!`).then(msg => {
+            const embed = new Discord.RichEmbed()
+            .setTitle(`Ping Statitics`)
+            .setDescription(`:ping_pong: My ping is ${Math.round(call.client.ping)}`)
+            .setFooter(`You ran this command on Linkcord.`)
+            msg.edit(embed);
+        });
     }
 };
