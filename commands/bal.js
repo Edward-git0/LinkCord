0793080ccf8c53ac1727421bcc0048359eefd45d
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-02-04 20:15:12 -0500

    Check your balance

---
+++ commands/bal.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+module.exports = {
+    id: 'bal',
+    aliases: ['balance', 'checkbalance', 'money', 'bank', 'linkcoins'],
+    channels: 'guild',
+    exec: (call) => {
+        try {
+			let user = call.message.author
+			let usersBalance = call.client.econData.get(`${user.id}-${call.message.guild.id}`, 'linkCoins')
+			const embed = new Discord.RichEmbed()
+			.setTitle(`Your LinkCoin Balance`)
+			.setDescription(`You currently have **${usersBalance}**<:linkcoin:670675326837194782> \n\nYou can visit the shop for rewards by running \`,shop\``)
+			call.message.channel.send(`⚒️ Please wait while I update your balance!`).then(msg => {
+				msg.edit(embed)
+			})
+		} catch(error) {
+            call.message.channel.send(`Oops! That was an error! The issue has been reported to the adminstration team`);
+            console.log(error);
+        }
+    }
+};
