9b08156d45ade058b88de2d8505646e16fe72334
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-02-10 19:29:01 -0500

    Cooldown database/Advertisements database

--- bot.js
+++ bot.js
@@ -30,37 +30,41 @@ client.on('ready', () => {
 	client.econData = newEnmap('economyData');
 	console.log(`The economyData database has been started.`);
 	client.cooldownData = newEnmap('cooldownData');
-	console.log('The cooldownData database has been started and is listening at localhost:8080');
+	console.log('The cooldownData database has been started');
 	console.log(`I am now ready for use! I am completely started.`);
+	console.log('Preparing the advertisements database.')
+	client.ads = newEnmap('advertisements')
 	console.log(`I am now intializing the systemData databse.`);
 	client.systemData = newEnmap('systemData');
 	console.log(`System Data is ready.`);
 });
 
 client.on('message', (message) => {
-	client.econData.ensure(`${message.author.id}-${message.guild.id}`, {
-		userID: message.author.id, 
-		linkCoins: 500, 
-		purchases: [], 
-		activityMeter: 0
-	})
-	client.cooldownData.ensure(`${message.author.id}-${message.guild.id}`, {
-		userID: message.author.id, 
-		activeCoolGuildID: message.guild.id,
-		commandsWithActiveCool: [], 
-		commandHashWithActiveCool: [], 
-		coolPardon: false,
-	});
-	client.systemData.ensure(message.author.id, {
-		userID: message.author.id, 
-		userBlocked: false, 
-		grantEvalAccess: false, 
-		grantEconAdmin: false, 
-		grantSuperPerms: false, 
-		grantEdwardIsADuck: false, 
-		grantDevCodeAccess: false, 
-		grantCodeExportAccess: false
-	});
+	if(message.channel.type === 'text') {
+		client.econData.ensure(`${message.author.id}-${message.guild.id}`, {
+			userID: message.author.id, 
+			linkCoins: 500, 
+			purchases: [], 
+			activityMeter: 0
+		})
+		client.cooldownData.ensure(`${message.author.id}-${message.guild.id}`, {
+			userID: message.author.id, 
+			activeCoolGuildID: message.guild.id,
+			commandsWithActiveCool: [], 
+			commandHashWithActiveCool: [], 
+			coolPardon: false,
+		});
+		client.systemData.ensure(message.author.id, {
+			userID: message.author.id, 
+			userBlocked: false, 
+			grantEvalAccess: false, 
+			grantEconAdmin: false, 
+			grantSuperPerms: false, 
+			grantEdwardIsADuck: false, 
+			grantDevCodeAccess: false, 
+			grantCodeExportAccess: false
+		});
+	}
 });
 
 
