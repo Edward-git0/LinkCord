bc26d274f9e4fecb6fc53cd63d08c10e0f8ddd3e
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-02-13 22:31:02 -0500

    command for removing permissions

---
+++ commands/denyEcoAdmin.js
@@ -0,0 +1,19 @@
+module.exports = {
+    id: 'denyecoadmin',
+    aliases: ['denecoad', 'denyeco'],
+    channels: 'any',
+    exec: (call) => {
+        try {
+		if(call.client.systemData.get(call.message.author.id, 'grantSuperPerms') === false) return;
+		let target = call.message.mentions.users.first();
+		if(!target) return call.message.channel.send(`Mention A user`)
+		call.message.channel.send(`Working on it.`).then(msg => {
+			call.client.systemData.set(target.id, false, 'grantEconAdmin')
+			msg.edit('Done.')
+		});
+        } catch(error) {
+            call.message.channel.send(`Oops! That was an error! The issue has been reported to the adminstration team`);
+            console.log(error);
+        }
+    }
+};
