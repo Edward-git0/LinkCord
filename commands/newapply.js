813950a6f40f951a3e5057531744d21a34b72458
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-02-14 18:19:16 +0000

    Added the legal agreement to the newapply command

--- commands/newapply.js
+++ commands/newapply.js
@@ -24,6 +24,17 @@ module.exports = {
         const usersDMs = call.message.author.createDM();
 
         call.message.channel.send(`Welcome to the LinkCord staff application! The application will now continue in your DMs. Please make sure they are open for me to DM.`)
+        
+        //Inital Prompt;;
+
+       await call.prompt(`Hello! Are you ready to begin your application to become LinkCord staff?`, { time: 30000, channel: usersDMs, filter: ['yes', 'no']}).then(initialQuestion => {
+         if(initialQuestion.content.toLowerCase() === 'no') {
+           call.message.author.send(`OK. Cancelling prompt.`)
+           return;
+         } else {
+           call.prompt(`Before we begin, please read and accept the following agreement. \nI, **${call.message.author.tag}**, agree that filling out this application in no way, will grant me a position at LinkCord or any of its affiliate(s) unless I am accepted as a member of the staff by a verified member of Administration. \nI, ${call.message.author.tag} also agree that if I am declined for a position at LinkCord or any of its affiliate(s), that it is the finald decision of the administration member if you are accepted or not.`)
+         }
+       });
 
 
 
