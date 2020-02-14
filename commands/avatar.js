f493c5bc30828109a2e9a2e12c2991792ea6f8db
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-02-13 22:30:26 -0500

    avatar command

---
+++ commands/avatar.js
@@ -0,0 +1,15 @@
+module.exports = {
+    id: 'avatar',
+    aliases: ['showavatar'],
+    channels: 'any',
+    exec: (call) => {
+        try {
+			let targetPerson = call.message.mentions.users.first() || call.client.users.get(call.args[0])
+			if(!targetPerson) targetPerson = call.message.author
+			call.message.channel.send(targetPerson.avatarURL);
+        } catch(error) {
+            call.message.channel.send(`Oops! That was an error! The issue has been reported to the adminstration team`);
+            console.log(error);
+        }
+    }
+}; 
