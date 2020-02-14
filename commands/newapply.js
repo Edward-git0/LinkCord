cfa625fffeff6d52c4ad85144d2973db5e2fdb21
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-02-14 18:01:21 +0000

    Making new apply command, fresher than the other one :)

--- commands/newapply.js
+++ commands/newapply.js
@@ -3,9 +3,28 @@ module.exports = {
     id: 'newapply',
     aliases: ['betaapply'],
     channels: 'guild/dm/any',
-    exec: (call) => {
+    exec: async (call) => {
         try {
-        let database = call.client.application
+        let database = call.client.staffapps
+        let applicationApprovalChannel = call.client.channels.get('677649993968123947');
+        // Question answer declarations.
+        let legalInquiryAnswer;
+        let question1Answer;
+        let question2Answer;
+        let question3Answer;
+        let question4Answer;
+        let question5Answer;
+        let question6Answer;
+        let question7Answer;
+        let question8Answer;
+        let question9Answer;
+        let question10Answer;
+
+
+        const usersDMs = call.message.author.createDM();
+
+        call.message.channel.send(`Welcome to the LinkCord staff application! The application will now continue in your DMs. Please make sure they are open for me to DM.`)
+
 
 
 
