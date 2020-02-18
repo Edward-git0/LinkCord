aefd7e020dc4b501d1b23d7be8fefc3c466ea8be
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-02-18 00:19:31 -0500

    broken

--- commands/newapply.js
+++ commands/newapply.js
@@ -2,17 +2,16 @@ const Discord = require('discord.js');
 module.exports = {
     id: 'newapply',
     aliases: ['betaapply'],
-    channels: 'guild/dm/any',
+    channels: 'any',
     exec: async (call) => {
         try {
         let database = call.client.staffapps
         let applicationApprovalChannel = call.client.channels.get('677649993968123947');
         // Question answer declarations.
-        let legalInquiryAnswer;
         let question1Answer;
         let question2Answer;
         let question3Answer;
-        let question4Answer;
+        let positionChoice;
         let question5Answer;
         let question6Answer;
         let question7Answer;
@@ -24,7 +23,7 @@ module.exports = {
         const question1String = `**Question 1:** How old are you?`
         const question2String = `**Question 2:** What timezone do you reside in?`
         const question3String = `**Question 3:** What do you think you bring to the staff team?`
-        const question4String = `**Question 4:** Which position are you applying for? \n*Available options: Community manager, moderator`
+        const question4String = `**Question 4:** Which position are you applying for? \n*Valid Reponses Are: community agent, moderator*`
         const question5CommunityString = `**Question 5C:** How friendly are you on a daily basis?`
         const question6CommunityString = `**Question 6C:** How do you handle stressful situations?`
         const question7CommunityString = `**Question 7C:** How many activities would you be able to arrange for our community?`
@@ -42,7 +41,7 @@ module.exports = {
         const question12ModSituational = `**Question 12MS:** Two staff members are fighting. How should you react? What actions should you take?`
 
 
-        const usersDMs = call.message.author.createDM();
+        const usersDMs = await call.message.author.createDM();
 
         call.message.channel.send(`Welcome to the LinkCord staff application! The application will now continue in your DMs. Please make sure they are open for me to DM.`)
         
@@ -50,35 +49,54 @@ module.exports = {
 
            //Legal agreement prompt;;
 
-          await call.prompt(`Before we begin, please read and accept the following agreement. \nI, **${call.message.author.tag}**, agree that filling out this application in no way, will grant me a position at LinkCord or any of its affiliate(s) unless I am accepted as a member of the staff by a verified member of Administration. \nI, ${call.message.author.tag} also agree that the decision of the status of your application is **final** and cannot be changed unless proof of unfair judgement is provided. \n\nDo you agree to these terms?`, { time: 45000, channel: usersDMs, filter: ['yes', 'no']}).then(legalAgreementMsg => {
-             if(legalAgreementMsg.content.toLowerCase() === 'no') return call.message.author.send(`:x: Your application was denied by ${call.client.user.tag} for reason __Didn't accept the legal agreement__`);
-           });
+           if (await call.prompt(`Before we begin, please read and accept the following agreement. \nI, **${call.message.author.tag}**, agree that filling out this application in no way, will grant me a position at LinkCord or any of its affiliate(s) unless I am accepted as a member of the staff by a verified member of Administration. \nI, **${call.message.author.tag}** also agree that the decision of the status of your application is **final** and cannot be changed unless proof of unfair judgement is provided. \n\nDo you agree to these terms?`, { time: 45000, channel: usersDMs, filter: ['yes', 'no']}).then((m) => m.content.toLowerCase() === 'no')) {
+            return call.message.author.send(`:x: Your application was denied by ${call.client.user.tag} for reason __Didn't accept the legal agreement__`);
+          }
         //End Legal Agreement prompt;;
 
         // Question 1 
-        await call.prompt(question1String, { time: 30000, channel: usersDMs}).then(question1Msg => {
+         await call.prompt(question1String, { time: 30000, channel: usersDMs}).then(question1Msg => {
           question1Answer = question1Msg.content;
         });
         //Question 1 end;
 
         //question 2;;
-        await call.prompt(question2String, { time: 30000, channel: usersDMs}).then(question2Msg => {
+         await call.prompt(question2String, { time: 30000, channel: usersDMs}).then(question2Msg => {
           question2Answer = question2Msg.content;
         });
         //question 2 end;;
 
+        //question 3 begin;
         await call.prompt(question3String, { time: 30000, channel: usersDMs}).then(question3Msg => {
-          //to be continued 
+            question3Answer = question3Msg.content;
         });
+        //question 3 end
+
+        //question 4 begin;
+
+        await call.prompt(question4String, { time: 45000, channel: usersDMs, filter: ['community agent', 'moderator'] }).then(positionInput => {
+          if(positionInput.content.toLowerCase() === 'community agent') {
+
+            await call.prompt(question5CommunityString, { time: 50000, channel: usersDMs}).then(question5CMsg => {
+              question5Answer = question5CMsg.content;
+             });
+              call.prompt(question6CommunityString, { time: 50000, channel: usersDMs }).then(question6CMsg => {
+               question5Answer = question6CMsg.content;
+             })
+          }
+          if(positionInput.content.toLowerCase() === 'moderator') {
+            positionChoice = positionInput.content;
+            call.messge.author.send('moderators are gay')
+          }
+
+        })
         
 
 
 
 
-
         } catch(error) {
-            call.message.channel.send(`Oops! That was an error! The issue has 
-            been reported to the adminstration team`);
+            call.message.channel.send(`Oops! That was an error! The issue has been reported to the adminstration team`);
             console.log(error);
         }
     }
