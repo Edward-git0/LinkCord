4433d8c0ff2791e60eb53691828c92d982b89169
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-02-19 15:10:40 +0000

    Start ad command

--- commands/ad.js
+++ commands/ad.js
@@ -3,13 +3,14 @@ module.exports = {
     id: 'ad',
     aliases: ['postad', 'advertisement'],
     channels: 'guild',
-    exec: (call) => {
+    exec: async (call) => {
         try {
-        call.message.channel.send(`Hello! Welcome to the ad prompt! Please wait while I prepare the command for you.`).then(msg => {
-            setTimeout(() => {
-                msg.edit(`I've setup the prompt for you! I'll be sending you a DM shortly to continue with the prompt. Please make sure your DM's are open, and I am unblocked.`)
-            }, 2000);
-        })
+        call.message.channel.send(`Hello! The advertisement prompt will continue in your DMs.`)
+
+        const usersDMs = await call.message.author.createDM().catch(error => return call.message.channel.send(`I couldn't open a DM, check if your DM's are open.`))
+        const titlePrompt = await call.prompt(`What would you like the title for your advertisement to be?`, { time: 60000, channel: usersDMs })
+
+        const embedBodyPrompt = await call.prompt(`What would you like the body of your embed to be?`, { time: 50000, channel: usersDMs })
         } catch(error) {
             call.message.channel.send(`Oops! That was an error! The issue has been reported to the adminstration team`);
             console.log(error);
