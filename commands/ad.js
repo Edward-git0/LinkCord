79d2733e524fa9072c9daaaf4e737db600b643b5
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-02-19 17:21:52 +0000

    ✅❎🧫🧬📡

--- commands/ad.js
+++ commands/ad.js
@@ -13,11 +13,11 @@ module.exports = {
           log.send(`${call.message.guild.defaultRole.toString()} I am warning you: ${call.message.author.tag} tried to run the advertisement command, but the ads channel was deleted. Fix it in the code`)
           call.message.channel.send(`Something went wrong! The adminstration has been notified and will fix it shortly.`)
         }
-        call.message.channel.send(`Hello! The advertisement prompt will continue in your DMs.`)
+       const initialMessage = await call.message.channel.send(`Hello! The advertisement prompt will continue in your DMs.`)
 
         const usersDMs = await call.message.author.createDM().catch(error => return call.message.channel.send(`I couldn't open a DM, check if your DM's are open.`))
         const titlePrompt = await call.prompt(`What would you like the title for your advertisement to be?`, { time: 60000, channel: usersDMs })
-
+        initialMessage.edit(`Prompt in progress`)
         const embedBodyPrompt = await call.prompt(`What would you like the body of your embed to be?`, { time: 50000, channel: usersDMs })
 
         const collectImagePrompt = await call.prompt(`Please send an image attachment of the image you would like on your advertisement.`, { time: 60000, channel: usersDMs })
@@ -25,6 +25,11 @@ module.exports = {
         const title = titlePrompt.content;
         const body = embedBodyPrompt.content;
         const imageURL = collectImagePrompt.attachments.first().url
+
+        initialPrompt.edit(`Prompt Finished. `)
+
+
+
 
 
         } catch(error) {
