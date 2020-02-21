10ce8df0f903a2d9a639ae20d7a2333e0bcec49f
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-02-20 21:39:44 -0500

    Basically finish, just need to add the actions

--- commands/shop.js
+++ commands/shop.js
@@ -1,42 +1,67 @@
 const Discord = require('discord.js');
 module.exports = {
     id: 'shop',
-    aliases: ['buythings', 'linkcoinshop', 'edward'],
+    aliases: ['buythings', 'linkcoinshop', 'edward', 'store'],
     channels: 'guild',
     exec: async (call) => {
         try {
 
             let filter = await call.client.shopData.filter(find => {
-                return find.guildID === call.message.guild.id && find.forSale === true
+                return find.guildID === call.message.guild.id & find.forSale === true
             });
             let possibleEmojis = [];
             filter.forEach(findReactions => {
                 possibleEmojis.push(findReactions.itemReactable)
             })
+
             let reactionFilter = (reaction, user) => {
-                return possibleEmojis.includes(reaction) && user.id === call.message.author.id
+                return possibleEmojis.includes(reaction.emoji.name) && user.id === call.message.author.id
             }
+
             const linkCoin = call.client.emojis.get('670675326837194782');
+
+
             if(filter.size === 0) return call.message.channel.send(`There are no items for sale right now.`)
+
+
             let embedDesc = '';
                 filter.forEach(found => {
                     embedDesc += `${found.itemReactable}** __${found.itemName}__** \nDescription: ${found.itemDesc} \nCost: ${found.itemCost} ${linkCoin} \nQuantity: ${found.itemQuan} \n\n` 
-                })
+                });
+
+
+
             const shopEmbed = new Discord.RichEmbed()
             .setTitle(`${linkCoin} LinkCoins Shop`)
             .setColor('BLURPLE')
             .setDescription(embedDesc);
             call.message.channel.send(`Click on the reactions below to purchase.`)
             let embedMessage = await call.message.channel.send(shopEmbed)
+
+
             possibleEmojis.forEach(emojis => {
                 embedMessage.react(emojis)
-            })
-            const collect = embedMessage.createReactionCollector(reactionFilter, { max: 1, time: 180000 })
+            });
 
-            collect.on('collect', (reaction, reactionCollector) => {
-                call.message.channel.send(reaction)
+
+            embedMessage.awaitReactions(reactionFilter, { max: 1, time: 180000})
+            .then(collected => {
+                let collectedEmoji = collected.first().emoji.name;
+
+                if(collectedEmoji === '💎') {
+                    call.message.reply(`This item's forSale status changed while you were entering the prompt. You have not recieved the item, nor has the balance been deducted. \nThis item's status was \`open\`, but it is now \`store closed for public users. Please try again when the bot is public\` `)
+                }
+                if(collectedEmoji === '🖼️') {
+                    call.message.reply(`This item's forSale status changed while you were entering the prompt. You have not recieved the item, nor has the balance been deducted. \nThis item's status was \`open\`, but it is now \`store closed for public users. Please try again when the bot is public\` `)
+                }
+                if(collectedEmoji === '🛃') {
+                    call.message.reply(`This item's forSale status changed while you were entering the prompt. You have not recieved the item, nor has the balance been deducted. \nThis item's status was \`open\`, but it is now \`store closed for public users. Please try again when the bot is public\` `)
+                }
             })
             
+
+
+
         } catch(error) {
             call.message.channel.send(`Oops! That was an error! The issue has been reported to the adminstration team`);
             console.log(error);
