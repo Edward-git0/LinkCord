c43d69bc434de6c73675f4771dfdb82a8595821f
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-03-01 10:19:45 -0500

    welcomembed

--- clientEvents/guildMemberAdd.js
+++ clientEvents/guildMemberAdd.js
@@ -1,5 +1,13 @@
-module.exports = (client, message) => {
-	//message event 
-  
-	//filter should go here 
-  };
+const Discord = require('discord.js')
+
+module.exports = (client, member) => {
+	let channel = client.channels.get('659179304135032843')
+	const welcomeembed = new Discord.RichEmbed()
+    .setTitle("Welcome to LinkCord!")
+    .setDescription(
+      `**Hi ${member}** :wave:, welcome to our server!\n\nJust a few quick things you should know;\n:information_source: You can find most of the information you'll need in <#660644421968068628>\n:clipboard: You can find all of our rules in <#658780835763191821>\n<:linkcord:660886312798257162> We keep the community updated in <#660923343133999105> about progress regarding the server\n\n\nWe hope you enjoy your stay at LinkCord and don't hesitate to ask our staff for help if you need it! :slight_smile: `
+    )
+    .setTimestamp()
+	.setColor("BLURPLE");
+	channel.send(welcomeembed)
+  }
