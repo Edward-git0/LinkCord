45789ea326e2d6202804f5ff240581ffbf3eaa86
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-03-01 10:24:04 -0500

    change prefix

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	token: 'NjgyMDU3NDY5OTMzMjU2NzE0.XlXdiQ.K7DvVNS1izzVPp0jxab38rYOPDI',
-	prefix: '--',
+	prefix: '~',
 	extraPerms: ['443664778901061633', '300816697282002946', '314991012600610817', '338509501290250240']
 }
