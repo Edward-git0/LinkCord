16088ca3bc72de6405e8a40479f7235ae0dbad12
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-03-03 17:10:53 +0000

    Basically finish mute command

--- commands/mute.js
+++ commands/mute.js
@@ -11,6 +11,7 @@ module.exports = {
             
             let muteRole = call.message.guild.roles.find(r => r.name === 'Muted')
             let reason = call.args[0];
+						let target = call.message.guild.members.get((call.args[0] || '').replace(/\D+/g, ''));
             if(!muteRole) {
                 call.client.channels.get('659149534894489639').send(`The muted role was deleted!!! I am creating a new one and updating the channel overrides.`)
                 muteRole = await call.message.guild.createRole({
@@ -26,10 +27,44 @@ module.exports = {
             }
 
             if(!reason)
-                return call.message.reply(`Invalid usage! ${call.prefixUsed}${call.command} [user mention/id] [reason]`)
+                return call.message.reply(`Invalid usage! \`${call.prefixUsed}${call.command} [user mention/id] [reason]\` `)
+				    if(!target) 
+							return call.message.rely(`Invalid user! \`${call.prefixUsed}${call.command} [user mention/id] [reason] \``)
+
+						call.message.member.addRole(muteRole, [`${call.message.author.tag} permanently muted them.`])
+						let caseNum = call.client.guildData.get(call.message.guild.id, 'lastcase') + 1;
+						call.client.guildData.set(call.message.guild.id, caseNum, 'lastcase');
+
+
+						call.client.moderationData.set(`${caseNum}-${call.message.guild.id}-${calledMember.user.id}`, {
+						userid: calledMember.id,
+						moderator: call.message.author.tag,
+						guildid: call.message.guild.id,
+						caseid: caseNum,
+						punishmenttype: 'mute',
+						punishmentreason: reason,
+						expiry: 'perm'
+						});
+
+						call.message.reply(`I have successfully muted ${target.tag} until January 1st, 2090 for ${reason}`)
+
+						target.send(`**You have been muted in LinkCord!** \nYou were muted by ${call.message.author.tag} for ${reason}`)
+
+
+
+					let loggingEmbed = new Discord.RichEmbed()
+					.setTitle(`${target.tag} was muted`)
+					.setDescription(`**Moderator:** ${call.message.author.tag} *(${call.message.author.id})* \n**Reason:** ${reason} \n**Case ID:** ${caseNum} \n**Expiry:** perm`)
+					.setFooter(`This user was muted permanently. They must be unmuted manually with the ?unmute command`)
+					.setTimestamp()
+					.setColor(`RED`)
+					call.client.channels.get('659149534894489639').send(loggingEmbed)
+
+
+						
 
         } catch(error) {
-            call.message.channel.send(`💥 Something went wrong while this command was executing! It has been reported to the developer team and it will be fixed soon.`);
+            call.message.channel.send(`💥 Something went wrong while this command was executing! It has been reported to the developer team and it will be fixed soon. 💥`);
             console.log(error);
         }
     }
