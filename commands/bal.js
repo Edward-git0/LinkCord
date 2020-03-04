df4583bf8961d1c4bacf836474be2536d233da98
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-03-03 19:29:46 -0500

    Make sure its showing the mentioned user + your own avatar in the balance command

--- commands/bal.js
+++ commands/bal.js
@@ -8,12 +8,12 @@ module.exports = {
 			let user = call.message.mentions.users.first()
 
 			if(!user) user = call.message.author
-			console.log(user.id)
 			let usersBalance = call.client.econData.get(`${user.id}-${call.message.guild.id}`, 'linkCoins')
 			const embed = new Discord.RichEmbed()
 			.setTitle(`Your LinkCoin Balance`)
 			.setDescription(`You currently have **${usersBalance}**<:linkcoin:670675326837194782> \n\nYou can visit the shop for rewards by running \`,shop\``)
-			.setImage(call.message.author.avatarURL)
+			.setImage(user.avatarURL)
+			.setColor('BLURPLE')
 			call.message.channel.send(`⚒️ Please wait while I update your balance!`).then(msg => {
 				msg.edit(embed)
 			})
