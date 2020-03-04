2fff8c0225939512939e7de56458f500d49fd7c8
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-03-03 19:29:56 -0500

    Complete mute command

--- commands/mute.js
+++ commands/mute.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 module.exports = {
-    id: 'name',
-    aliases: ['alias array'],
-    channels: 'guild/dm/any',
+    id: 'mute',
+    aliases: ['m'],
+    channels: 'guild',
     exec: async (call) => {
         try {
 
@@ -10,8 +10,8 @@ module.exports = {
                 return;
             
             let muteRole = call.message.guild.roles.find(r => r.name === 'Muted')
-            let reason = call.args[0];
-						let target = call.message.guild.members.get((call.args[0] || '').replace(/\D+/g, ''));
+            let reason = call.args.splice(1).join(' ')
+			let target = call.message.guild.members.get((call.args[0] || '').replace(/\D+/g, ''));
             if(!muteRole) {
                 call.client.channels.get('659149534894489639').send(`The muted role was deleted!!! I am creating a new one and updating the channel overrides.`)
                 muteRole = await call.message.guild.createRole({
@@ -27,17 +27,17 @@ module.exports = {
             }
 
             if(!reason)
-                return call.message.reply(`Invalid usage! \`${call.prefixUsed}${call.command} [user mention/id] [reason]\` `)
-				    if(!target) 
-							return call.message.rely(`Invalid user! \`${call.prefixUsed}${call.command} [user mention/id] [reason] \``)
+                return call.message.reply(`Invalid usage! \`${call.prefixUsed}${call.command.id} [user mention/id] [reason]\` `)
+            if(!target) 
+				return call.message.reply(`Invalid user! \`${call.prefixUsed}${call.command.id} [user mention/id] [reason] \``)
 
-						call.message.member.addRole(muteRole, [`${call.message.author.tag} permanently muted them.`])
+						target.addRole(muteRole, [`${call.message.author.tag} permanently muted them.`])
 						let caseNum = call.client.guildData.get(call.message.guild.id, 'lastcase') + 1;
 						call.client.guildData.set(call.message.guild.id, caseNum, 'lastcase');
 
 
-						call.client.moderationData.set(`${caseNum}-${call.message.guild.id}-${calledMember.user.id}`, {
-						userid: calledMember.id,
+						call.client.moderationData.set(`${caseNum}-${call.message.guild.id}-${target.user.id}`, {
+						userid: target.id,
 						moderator: call.message.author.tag,
 						guildid: call.message.guild.id,
 						caseid: caseNum,
@@ -46,7 +46,7 @@ module.exports = {
 						expiry: 'perm'
 						});
 
-						call.message.reply(`I have successfully muted ${target.tag} until January 1st, 2090 for ${reason}`)
+						call.message.reply(`I have successfully muted ${target.user.tag} until January 1st, 2090 for ${reason}`)
 
 						target.send(`**You have been muted in LinkCord!** \nYou were muted by ${call.message.author.tag} for ${reason}`)
 
