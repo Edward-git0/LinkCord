ac82b98ea93f133d3ddf381394bab622e50ee7d2
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-03-09 18:33:41 -0400

    Update add.js

--- commands/add.js
+++ commands/add.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 module.exports = {
     id: 'add',
+    category: 'developer',
+	enabled: true,
     aliases: ['baladd', 'balanceadd', 'configadduserbal'],
     channels: 'guild',
     exec: (call) => {
