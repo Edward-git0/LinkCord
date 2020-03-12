a8f1727825766395b1d5be724e746c42d098add2
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-03-11 20:57:10 -0400

    start help command

--- commands/testhelp.js
+++ commands/testhelp.js
@@ -8,10 +8,32 @@ module.exports = {
     channels: 'guild',
     exec: (call) => {
         try {
-        let find = call.commands.filter(c => {
-			return c.category === '.' && c.enabled === true
-        })
+        
 
+        let permissionLevel;
+        let find;
+        if(call.message.member.roles.has('658837632066912276')) {
+            console.log('admin')
+            permissionLevel = 'admin'
+            find = call.commands.filter(c => {
+                return c.category === 'public' || c.category === 'developer' || c.category === 'staff'
+            });
+        }
+
+        if(call.message.member.roles.has('659011063312023572') || call.message.member.roles.has('658867543120805898')) {
+            console.log('staff')
+            permissionLevel = 'staff'
+            find = call.commands.filter(c => {
+                return c.category === 'public' || c.category === 'staff' & c.enabled === true
+            });
+
+            if(!call.message.member.hasPermission('KICK_MEMBERS')) {
+                permissionLevel = 'member'
+                find = call.commands.filter(c => {
+                    return c.category === 'public' && c.enabled === true
+                });
+            }
+        }
         if(find.size === 0)
             return call.message.channel.send(`There are no commands available for view. You may be blacklisted or the bot may be under maintenance.`)
         
@@ -21,6 +43,7 @@ module.exports = {
         })
 
         call.message.channel.send(message)
+        call.message.channel.send(`You viewing commands for ${permissionLevel}`)
 		call.message.channel.send(`I found ${find.size}`)
         } catch(error) {
             call.message.channel.send(`💥 Something went wrong while this command was executing! It has been reported to the developer team and it will be fixed soon.`);
