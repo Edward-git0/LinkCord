9ebc05c8f37b19a4e77e3c4e60d3de763cd8843c
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-03-11 20:55:45 -0400

    Ad filter

--- clientEvents/message.js
+++ clientEvents/message.js
@@ -1,5 +1,15 @@
+const Discord = require('discord.js')
 module.exports = (client, message) => {
-  //message event 
-
-	//filter should go here 
+	let regx = /(https?:\/\/)?(www\.)?(discord\.(gg|io|me|li)|discordapp\.com\/invite)\/.+[a-z]/g
+  if(regx.test(message.content.toLowerCase().replace(/\s+/g, ''))) {
+		message.delete();
+		message.reply(`⚠️ Invite links to other servers are not permitted in this channel. Please submit an advertisement witht the \`~ad\` command. ⚠️`)
+		let embed = new Discord.RichEmbed()
+		.setTitle('⚠️ An automoderation warning was issued')
+		.setDescription(`A moderation action was taken against **${message.author.tag}** *(${message.author.id})* for their message in ${message.channel.toString()} with the content of` + '```' + message.content + '```')
+		.setTimestamp()
+		.setColor('YELLOW')
+		.setFooter(`LinkCord Moderation ~ Warning`, message.author.avatarURL)
+		client.channels.get('659149534894489639').send(embed)
+  }
 };
