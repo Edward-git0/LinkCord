ff14c43d80124ccc78260ea4591c1a004286dd0f
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-03-15 19:44:30 -0400

    ..

--- clientEvents/message.js
+++ clientEvents/message.js
@@ -1,9 +1,14 @@
 const Discord = require('discord.js')
 module.exports = (client, message) => {
-	let regx = /(https?:\/\/)?(www\.)?(discord\.(gg|io|me|li)|discordapp\.com\/invite)\/.+[a-z]/g
+
+	// if (message.author.id === '443664778901061633') {
+	// 	if (message.attachments.size > 0)
+	// 		return message.delete()
+	// }
+  let regx = /(https?:\/\/)?(www\.)?(discord\.(gg|io|me|li)|discordapp\.com\/invite)\/.+[a-z]/g
   if(regx.test(message.content.toLowerCase().replace(/\s+/g, ''))) {
 		message.delete();
-		message.reply(`⚠️ Invite links to other servers are not permitted in this channel. Please submit an advertisement witht the \`~ad\` command. ⚠️`)
+		message.reply(`⚠️ Invite links to other servers are not permitted in this channel. Please submit an advertisement witht the \`~ad\` command. `)
 		let embed = new Discord.RichEmbed()
 		.setTitle('⚠️ An automoderation warning was issued')
 		.setDescription(`A moderation action was taken against **${message.author.tag}** *(${message.author.id})* for their message in ${message.channel.toString()} with the content of` + '```' + message.content + '```')
