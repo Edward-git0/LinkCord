63c2c4e19628923fc369fc9bd33e12212bf47bba
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-03-16 12:25:09 -0400

    Start rob.js

--- commands/rob.js
+++ commands/rob.js
@@ -10,17 +10,17 @@ module.exports = {
         try {
 		
 		let target = call.message.guild.members.get((call.args[0] || '').replace(/\D+/g, ''));
-		
+		//Check if there is no target
 		if(!target)
 			return call.message.channel.send(`You have to find someone to rob first!`)
-		
+		//check If the user is robbing themselves.
 		if(target.id === call.message.author.id)
 			return call.message.channel.send(`Its illogical to rob yourself. Why would you even consider that?`)
-		
+		//check if the user they are robbing is a bot
 		if(target.user.bot) 
 			return call.message.channel.send(`Bots have no money for you, move on!`)
 		
-		
+		//Define the keys for the database so I don't have to write them over and over and over and over and over and over and over and over and over and over and over
 		let mKey = `${call.message.author.id}-${call.message.guild.id}`
 		let tKey = `${target.id}-${call.message.guild.id}`
 		let targetBalance;
@@ -32,8 +32,18 @@ module.exports = {
 			return call.message.channel.send(`That user has nothing to rob! Tell them to chat a little.`)
 		}
 
-		call.message.channel.send(`to be continued`)
 
+		//Calculate prob if they arent members 
+			//PUT HERE SO IT DOESNT GET OVERWRITTEN
+			//TO DO
+		if(call.message.member.roles.has(call.message.guild.roles.find(r => r.name === '💎 Diamond'))) {
+			//Calc prob if they are diamond
+			//To do
+		}
+		if(call.message.member.roles.has(call.message.guild.roles.find(r => r.name === '💎 Diamond+'))) {
+			//Calc prob if they are diamond+
+			//To do
+		}
 
         } catch(error) {
             let em = call.client.emojis.get('687791419766734930')
