64e4bba9e4ba8b3b8fb8034c28dc6188e6c1d2c6
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-03-17 22:33:25 -0400

    Put all of the infastructure into the rob command

--- commands/rob.js
+++ commands/rob.js
@@ -6,16 +6,17 @@ module.exports = {
     category: 'public',
     enabled: true, 
     channels: 'guild',
-    exec: (call) => {
+    exec: async (call) => {
         try {
-		
 		let target = call.message.guild.members.get((call.args[0] || '').replace(/\D+/g, ''));
+		let linkcoin = call.client.emojis.get('670675326837194782')
 		//Check if there is no target
 		if(!target)
 			return call.message.channel.send(`You have to find someone to rob first!`)
+		
 		//check If the user is robbing themselves.
 		if(target.id === call.message.author.id)
-			return call.message.channel.send(`Its illogical to rob yourself. Why would you even consider that?`)
+			return call.message.channel.send(`It's illogical to rob yourself. Why would you even consider that?`)
 		//check if the user they are robbing is a bot
 		if(target.user.bot) 
 			return call.message.channel.send(`Bots have no money for you, move on!`)
@@ -24,6 +25,9 @@ module.exports = {
 		let mKey = `${call.message.author.id}-${call.message.guild.id}`
 		let tKey = `${target.id}-${call.message.guild.id}`
 		let targetBalance;
+		let prob;
+		let poss = Math.random()
+		let looted;
 		
 		// Try to get the targets balance from the database, if that fails they don't have any userdata and must chat before being robbed.
 		try {
@@ -32,17 +36,58 @@ module.exports = {
 			return call.message.channel.send(`That user has nothing to rob! Tell them to chat a little.`)
 		}
 
-
+		if(targetBalance < 50)
+			return call.message.channel.send(`Its not worth it.. Move on.`)
 		//Calculate prob if they arent members 
-			//PUT HERE SO IT DOESNT GET OVERWRITTEN
-			//TO DO
-		if(call.message.member.roles.has(call.message.guild.roles.find(r => r.name === '💎 Diamond'))) {
-			//Calc prob if they are diamond
-			//To do
+		prob = 0.90
+		if(call.message.member.roles.has(call.message.guild.roles.find(r => r.name === '💎 Diamond').id)) {
+			prob = 0.85
 		}
-		if(call.message.member.roles.has(call.message.guild.roles.find(r => r.name === '💎 Diamond+'))) {
-			//Calc prob if they are diamond+
-			//To do
+		if(call.message.member.roles.has(call.message.guild.roles.find(r => r.name === '💎 Diamond+').id)) {
+			prob = 0.75
+		}
+
+		let message = await call.message.channel.send(`You are beating up ${target} for their money..`)
+		let failedResponses = [
+			`${target.user.username} gave you a purple-nurple and sprayed you with pepper-spray`,
+			`${target.user.username} kicked you where the sun doesn't shine`,
+			`${target.user.username} gave you a wedgie and sprayed you with a lemon!`,
+			`${target.user.username} called the police and they fined you!`,
+			`${target.user.username} hit you over the head with a garbage can lid and kicked where the sun doesn't shine!`,
+			`${target.user.username} hit you over the head with a garbage can lid`,
+			`${target.user.username} shoved a raccoon in your face!`,
+			`${target.user.username} hit you with their fanny pack!`,
+			`${target.user.username} punched you and knocked you out`,
+			`${target.user.username} Roundhouse kicked you right in the face!`,
+			`${target.user.username} pushed you into a garbage can!`,
+			`${target.user.username} shoved your face into a brick wall`,
+			`${target.user.username} shoved your face into a pie!`
+
+		]
+		if(poss < prob) {
+			let failed = new Discord.RichEmbed()
+			.setTitle(`${failedResponses[Math.floor(Math.random() * failedResponses.length)]}`)
+			.setDescription(`You tried to rob ${target}, but instead you lost \`50\` ${linkcoin}`)
+			.setFooter(`LinkCord Economy ~ You lost a robbery`)
+			.setTimestamp()
+			.setColor('RED')
+			message.edit(failed)
+		} else {
+			looted = Math.floor(Math.random() * (150 - 75) + 75);
+			if(call.message.member.roles.has(call.message.guild.roles.find(r => r.name === '💎 Diamond').id)) {
+				looted = Math.floor(Math.random() * (300 - 100) + 100)
+			}
+			if(call.message.member.roles.has(call.message.guild.roles.find(r => r.name === '💎 Diamond+').id)) {
+				looted = Math.floor(Math.random() * (850 - 400) + 400);
+			}
+			let won = new Discord.RichEmbed()
+			.setTitle(`You won the robbery!`)
+			.setTitle(`You were able to loot \`${looted}\` ${linkcoin} from ${target}!`)
+			.setFooter(`Want more loot? run ${call.prefixUsed}store and buy Diamond!`)
+			.setColor('RED')
+			.setThumbnail('https://giphy.com/gifs/swag-money-make-it-rain-2u11zpzwyMTy8')
+			message.edit(won)
+			
 		}
 
         } catch(error) {
