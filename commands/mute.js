d2bf3a3af767e30bc4c30829faec55c6c1c7b218
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-03-31 15:34:41 -0400

    rename tempmute -> mute

--- commands/mute.js
+++ commands/mute.js
@@ -1,20 +1,23 @@
 const Discord = require('discord.js');
+const moment = require('moment');
+const ms = require('ms');
 module.exports = {
     id: 'mute',
     aliases: ['m'],
     category: 'staff',
     enabled: true,
-    desc: 'Allows staff to mute users.',
+    desc: 'Allows staff to mute users for a specified amount of time',
     channels: 'guild',
     exec: async (call) => {
         try {
 
             if(!call.message.member.hasPermission('MUTE_MEMBERS'))
                 return;
-            
+            call.message.delete()
             let muteRole = call.message.guild.roles.find(r => r.name === 'Muted')
-            let reason = call.args.splice(1).join(' ')
-			let target = call.message.guild.members.get((call.args[0] || '').replace(/\D+/g, ''));
+            let reason = call.args.splice(2).join(' ')
+            let target = call.message.guild.members.get((call.args[0] || '').replace(/\D+/g, ''));
+            let time = call.args[1]
             if(!muteRole) {
                 call.client.channels.get('659149534894489639').send(`The muted role was deleted!!! I am creating a new one and updating the channel overrides.`)
                 muteRole = await call.message.guild.createRole({
@@ -24,17 +27,22 @@ module.exports = {
                 call.message.guild.channels.forEach(async c => {
                     c.overwritePermissions(muteRole, {
                         SEND_MESSAGES: false,
-                        ADD_REACTIONS: false
+                        ADD_REACTIONS: false,
+                        SPEAK: false
                     });
                 });
             }
 
             if(!reason)
-                return call.message.reply(`Invalid usage! \`${call.prefixUsed}${call.command.id} [user mention/id] [reason]\` `)
+                return call.message.reply(`Invalid usage! \`${call.prefixUsed}${call.command.id} [user mention/id] [time] [reason]\` `)
             if(!target) 
 				return call.message.reply(`Invalid user! \`${call.prefixUsed}${call.command.id} [user mention/id] [reason] \``)
 
-						target.addRole(muteRole, [`${call.message.author.tag} permanently muted them.`])
+            if(!time)
+                return call.message.channel.send(`Please specify a time to mute ${target.tag} for.`)
+            
+            
+						target.addRole(muteRole, [`${call.message.author.tag} muted them for ${time}`])
 						let caseNum = call.client.guildData.get(call.message.guild.id, 'lastcase') + 1;
 						call.client.guildData.set(call.message.guild.id, caseNum, 'lastcase');
 
@@ -44,31 +52,32 @@ module.exports = {
 						moderator: call.message.author.tag,
 						guildid: call.message.guild.id,
 						caseid: caseNum,
-						punishmenttype: 'mute',
+                        punishmenttype: 'mute',
+                        reason: reason,
 						punishmentreason: reason,
-                        expiry: 'perm'
+                        expiry: Date.now() + ms(time),
+                        punishmentRemoved: false
 						});
-
                         let channelEmbed = new Discord.RichEmbed()
                         .setTitle(`I have successfully muted ${target.user.tag}!`)
-                        .setDescription(`**${target.user.tag} was muted for __${reason}__.**\nThis is case #${caseNum}`)
-                        .setFooter(`This mute will never expire ~ LinkCord Moderation`)
+                        .setDescription(`**${target.user.tag} was muted for __${reason}__.**\nTheir mute will expire on ${moment(Date.now() + ms(time)).format('MMMM Do YYYY, h:mm:ss a')}  \nThis is case #${caseNum}`)
+                        .setFooter(`LinkCord Moderation`)
                         .setColor('ORANGE')
 						call.message.reply(channelEmbed)
-						target.send(`**You have been muted in LinkCord!** \nYou were muted by ${call.message.author.tag} for ${reason}`)
+
+						target.send(`**You have been muted in LinkCord!** \nYou were muted by ${call.message.author.tag} for ${reason} for ${time}`)
 
 
 
-					let loggingEmbed = new Discord.RichEmbed()
-					.setTitle(`${target.user.tag} was muted`)
-					.setDescription(`**Moderator:** ${call.message.author.tag} *(${call.message.author.id})* \n**Reason:** ${reason} \n**Case ID:** ${caseNum} \n**Expiry:** perm`)
-					.setFooter(`This user was muted permanently. They must be unmuted manually with the ?unmute command`)
-					.setTimestamp()
-					.setColor(`RED`)
-					call.client.channels.get('659149534894489639').send(loggingEmbed)
+                        let embed = new Discord.RichEmbed()
+                        .setTitle('⚠️ An tempmute was issued')
+                        .setDescription(`A tempmute action was taken against ${target.user.tag} by ${call.message.author.tag} for reason __${reason}__. \n This mute will expire within 25s of ${moment(Date.now() + ms(time)).format('MMMM Do YYYY, h:mm:ss a')} \n**This is case #${caseNum}**`)
+                        .setTimestamp()
+                        .setColor('RED')
+                        .setFooter(`LinkCord Moderation ~ Action`, call.message.author.avatarURL)
+                        call.client.channels.get('659149534894489639').send(embed)
 
 
-						
 
         } catch(error) {
             call.message.channel.send(`💥 Something went wrong while this command was executing! It has been reported to the developer team and it will be fixed soon. 💥`);
