3b3f11ac430fa0b934a324e5aab600c95179e67d
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-03-31 15:35:21 -0400

    start acceptad

--- commands/acceptad.js
+++ commands/acceptad.js
@@ -1,15 +1,39 @@
 const Discord = require('discord.js');
 module.exports = {
     id: 'acceptad',
-    category: 'moderator',
+    category: 'staff',
 	enabled: true,
     aliases: [],
     channels: 'guild',
     exec: (call) => {
         try {
-            //code goes here
+            if(call.message.channel.name !== 'ad-approval') {
+                console.log('not ad approval')
+                return;
+            }
+            
+            let id = call.args[0]
+            if(!id)
+                return call.message.channel.send('Provide an ID.')
+            let ad = call.client.ads.find(a => {
+                return a.status === 'waiting' && a.adID === id
+            })
+
+            call.client.emit('adAccepted', ad.adID)
+
+            call.message.delete()
+            // call.message.channel.fetchMessage(ad.logChannelID).then(msg => {
+            //     msg.delete();
+            // }).catch(() => {
+            //     call.message.reply(`Approved the advertisement, but unable to delete the log message. Please delete it manually.`).then(msg => {
+            //         msg.delete(7000)
+            //     })
+            // })
+            
+
+            
         } catch (error) {
-            call.message.channel.send(`Oops! That was an error! The issue has been reported to the adminstration team`);
+            call.message.channel.send(`:x:`);
             console.log(error);
         }
     }
