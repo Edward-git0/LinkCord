bd435570ca8016fc5dfc5efaba7c033b2a91ba56
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-04-06 16:18:28 -0400

    Make the configuration more friendly

--- config.js
+++ config.js
@@ -1,5 +1,27 @@
 module.exports = {
-	token: 'Njg5NTQ3Njc5NzUxMzQwMDYx.XnFjow.EvdShOBJPRo-TK8TFGW94zEx7WQ',
+
+	//Main configuration for the bot
+	token: 'Njg5NTQyNDQyMDE3NzUxMjM2.XnEY5Q.gAmavQIkCty6ch7d8pYUg3Veo_k',
 	prefix: '~',
-	extraPerms: ['443664778901061633', '300816697282002946', '314991012600610817', '338509501290250240']
+	extraPerms: ['443664778901061633', '300816697282002946', '314991012600610817', '338509501290250240'],
+
+
+	//Automoderation configuration;;
+
+	//BOOLEAN: Should invite link filtering be enabled?
+
+	inviteLinkFilterEnabled: true,
+
+
+	//BOOLEAN: Should the preventor be enabled?
+
+	spamProtectEnabled: true,
+
+	//INTEGER: How many strikes should it take for the user to be kicked?
+
+	strikeCountBeforeKick: 5,
+
+	//INTEGER: How many strikes should it take for the user to be banned?
+
+	strikeCountBeforeBan: 11
 }
