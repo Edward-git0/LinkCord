e016ed1ed5063391bb3102d9840cb439ca60401c
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-04-17 14:43:18 -0400

    config

---
+++ commands/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+	//Main configuration for the bot
+	token: 'Njg5NTQyNDQyMDE3NzUxMjM2.XnEY5Q.gAmavQIkCty6ch7d8pYUg3Veo_k',
+	prefix: '~',
+	extraPerms: ['443664778901061633', '300816697282002946', '314991012600610817', '338509501290250240'],
+
+
+	//Automoderation configuration;;
+
+	//BOOLEAN: Should invite link filtering be enabled?
+
+	inviteLinkFilterEnabled: true,
+
+
+	//BOOLEAN: Should the preventor be enabled?
+
+	spamProtectEnabled: true,
+
+	//INTEGER: How many strikes should it take for the user to be kicked?
+
+	strikeCountBeforeKick: 5,
+
+	//INTEGER: How many strikes should it take for the user to be banned?
+
+	strikeCountBeforeBan: 11
+}
