7aa9df3ff26155f6799ab6aa669b3cd6f71cebda
thetechguy61705 <36977709+thetechguy61705@users.noreply.github.com>
2020-04-17 14:45:09 -0400

    Update sudo.js

--- commands/sudo.js
+++ commands/sudo.js
@@ -7,7 +7,7 @@ module.exports = {
 	desc: 'Makes the bot think the user sent the message (with the content specified) to the current channel.',
 	exec: async (call) => {
 
-		if (call.message.member.hasPermission('MANAGE_GUILD')) {
+		if (call.message.member.hasPermission('MANAGE_SERVER')) {
 			let author = call.client.users.get(((call.args[0] || '').match(/\d+/) || [])[0]);
 			if (author != null) {
 				let content = call.message.content.substring(call.prefixUsed.length).trim().substring(5 + call.args[0].length).trim();
